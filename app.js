8b259ca7bd56c0388315bd4cf944507c2218dcda
farnoud lavasani <farnoudlavasani@farnouds-MacBook-Pro.local>
2023-12-09 16:08:06 +0330

    edit button

--- app.js
+++ app.js
@@ -16,11 +16,25 @@ if (taskText !== ''){
     
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', function(event){
+    deleteButton.addEventListener('click', function(){
         taskList.removeChild(listItem);
+
+        
     })
+    const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', function(){
+            const newText = prompt('Edit task', listItem.textContent)
+            if (newText !== null){
+                listItem.textContent = newText;
+            }
+     
+        });
+
 listItem.appendChild(deleteButton);
+listItem.appendChild(editButton);
 taskList.appendChild(listItem);
+
 taskInput.value = '';
 } 
 
